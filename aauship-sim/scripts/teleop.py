a0aa9e848dc01e9071f9a2c72759c7d4a9eafe81
jaynom <jepnel@gmail.com>
2020-03-06 15:39:12 +0100

    teleoperation

---
+++ aauship-sim/scripts/teleop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+rospy.init_node('teleop')
+pub = rospy.Publisher('key_vel', Twist, queue_size = 1)
+msg_out = Twist()
+
+
+while(True):
+    msg_in = rospy.wait_for_message('joy', Joy)
+    msg_out.linear.x = msg_in.axes[1] - (msg_in.axes[5] -1)*10
+    msg_out.angular.z = -msg_in.axes[0]
+    pub.publish(msg_out)
+     
+
